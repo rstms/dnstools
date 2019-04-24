3b68561fab4d298f26cf500a83a2391c25057af8
Matt Krueger <mkrueger@rstms.net>
2019-04-23 21:52:00 -0500

    added dhcphosts command

---
+++ dnstools/commands/dhcphosts.py
@@ -0,0 +1,21 @@
+import click
+from dnstools.cli import pass_context
+from isc_dhcp_leases import IscDhcpLeases
+
+import platform
+
+if platform.system()=='OpenBSD':
+   LEASE_FILE='/var/db/dhcpd.leases'
+else:
+   LEASE_FILE='/var/lib/dhcpd/dhcpd.leases'
+
+@click.command()
+@pass_context
+@click.argument('domain')
+@click.argument('leasefile', default=LEASE_FILE)
+def cli(ctx, leasefile, domain):
+    """output active dhcp clients from dhcpd.leases in tinydns-data format"""
+    leases=IscDhcpLeases(leasefile)
+    for r in leases.get_current().values():
+        if r.hostname:
+            ctx.echo('=%s.%s:%s' % (r.hostname, domain, r.ip)) 
